ceebbc37d78b23ba93c29e3cf56c5c5e5bf2fbe3
Maxim <copperious@mail.ru>
2025-01-28 10:46:30 +0800

    Add new function

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
+
 @app.get("/items")
 def get_items():
     return {"items": ["item1", "item2", "item3"]}
+
+
+@app.get("/item_by_id")
+def get_item_by_id(id):
+    return {id: "items"}
