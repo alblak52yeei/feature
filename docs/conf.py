f6447b0a52d41ee2ec99d4563d82e43ea0e9641f
Maxim <copperious@mail.ru>
2025-03-04 19:07:34 +0800

    fix

---
+++ docs/conf.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+# Добавление пути к вашему проекту
+sys.path.insert(0, os.path.abspath('.'))
+
+# Основные настройки
+project = 'feature'
+author = 'Maxim'
+release = '1.0'
+
+# Расширения Sphinx
+extensions = [
+    'sphinx.ext.autodoc',  # Автоматическая документация кода
+    'sphinx_rtd_theme',    # Тема Read the Docs
+]
+
+# Тема оформления
+html_theme = 'sphinx_rtd_theme'
+
+# Путь к исходным файлам документации
+source_suffix = {
+    '.rst': 'restructuredtext',
+}
